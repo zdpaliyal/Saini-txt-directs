56bcd09f5889d51eb2a62ceff5e64a050161412e
Saini bots <tmglcd@gmail.com>
2025-05-13 14:08:51 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,10 +1,11 @@
 #🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
 # Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
 from os import environ
 
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(os.environ.get("OWNER", "5680454765"))
+OWNER = int(environ.get("OWNER", "5680454765"))
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
