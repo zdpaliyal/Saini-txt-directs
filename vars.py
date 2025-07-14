d6c0e01d52ad995e8c05bb18f4aa7d60eac9137c
zdpaliyal <zdpaliyal@gmail.com>
2025-07-14 19:28:06 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ from os import environ
 API_ID = int(environ.get("API_ID", "xxxxxxxx"))
 API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxx")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
+OWNER = int(environ.get("OWNER", "8024920136"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
 LOG_CHANNEL = "-1002631537674" # log group me bot ko admin narur banaye full rights ke sath
 #WEBHOOK = True  # Don't change this
